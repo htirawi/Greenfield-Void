a3125e196899d01a6f1c7f56460bdbdc213e403b
unknown <admin@originality.edco.jo>
2018-04-21 03:48:07 +0300

    hashing

---
+++ server/helpers/helpers.js
@@ -0,0 +1,39 @@
+// var bcrypt = require('bcrypt-nodejs');
+// var Promise = require('bluebird');
+
+
+var isLoggedIn = function(req) {
+  return req.session ? !!req.session.user : false;
+};
+
+exports.checkUser = function(req, res, next){
+  if (!isLoggedIn(req)) {
+    res.redirect('/signin');
+  } else {
+    next();
+  }
+};
+
+exports.createSession = function(req, res, newUser) {
+  return req.session.regenerate(function() {
+      req.session.user = newUser;
+      res.redirect('/');
+    });
+};
+
+
+// exports.comparePassword = function(candidatePassword, cb) {
+//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//         if (err) return cb(err);
+//         cb(null, isMatch);
+//     });
+
+
+
+// exports.hashPassword= function(password) {
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const db = require('../database/index.js')
 const session = require('express-session')
 const cookieParser = require('cookie-parser');
+const helper = require('./helpers/helpers.js')
 
 
 const app = express();
@@ -21,21 +22,47 @@ app.use(express.static(__dirname + '/../node_modules'));
 //   saveUninitialized: true
 // }));
 
+app.use(session({
+	secret: 'shhh, it\'s aa secret',
+	resave : false,
+	saveUninitialized:true
+}));
+
 app.get('/', function (req, res) {
  res.render('index.html')
 });
 
 app.post('/signin', function(req,res) {
-
+	//console.log(req.body.username)
+	var username = req.body.username;
+	var password = req.body.password;
+	var user = new db.User({'user':username,'password':password});
+	db.User.findOne({user:user.user},function(err,user){
+		if (err){console.log(err)}
+		else if(!user){console.log('user not found')}
+			// else{
+			// 	db.comparePassword(password,function(match){
+			// 		if(match){
+			// 		helper.createSession(req,res,user)
+			// 	}else{
+			// 		res.redirect('/signin')
+			// 	}
+			// 	})
+			// }
+	})
+	
+	//res.send()
 
 });
+
+
 
 app.post('/signup', function(req,res) {
 	var name = req.body.username
 	var password = req.body.password
     var email = req.body.email
 	var obj = {'user':name , 'password':password,'email':email}
-	console.log(obj)
+	//console.log(obj)
 	db.save(obj)
 
 });
