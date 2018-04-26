083b57ae6df45af39d0c32c5e97741988fedb997
alaamigdady <alaamohammad283@gmail.com>
2018-04-26 14:38:24 +0300

    test

--- server/index.js
+++ server/index.js
@@ -245,6 +245,11 @@ app.get('/logout', function(req, res) {
 
 //for deployment
 
+var port = process.env.PORT || 3000;
+
+const server = app
+.use((req, res) => res.render('index') )
+.listen(port, () => console.log(`Listening on ${ port }`));
 
 //Listen on port 3000
 // server = app.listen(3000)
@@ -292,6 +297,3 @@ socket.on('typing',function(data){
 // app.listen(3000, function() {
 // 	console.log('listening on port 3000!');
 // });
-var port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on ${ port }`));
